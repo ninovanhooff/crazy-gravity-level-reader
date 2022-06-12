91ccc41a13230a26e6e9a41186247f39ce1880a4
Nino van Hooff <nino@q42.nl>
2022-06-12 17:32:28 +0200

    ADD start playdate simulator on build

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e # exit on error
-FILENAME="nino4"
+FILENAME="User001"
 clear
 echo "--- COMPILING cglreader"
 ./compile.sh
 echo "--- READING specials & levelProps"
 ./run.sh $FILENAME
+echo "--- Copying levels to game sources"
+cp -R ./lua-levels/* ../CrazyGravityPlaydate/Source/levels
+echo "--- Opening Simulator"
+pdc ../CrazyGravityPlaydate/Source /tmp/cgpd.pdx && open /tmp/cgpd.pdx
