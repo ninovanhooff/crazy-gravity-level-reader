0de028b5f018857464b1e49365d0d57c9583e923
Nino van Hooff <nino@q42.nl>
2022-06-26 22:03:26 +0200

    ADD sample parallel command for sequence

--- process_all.sh
+++ process_all.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# all files
 find ./test-levels -type f -iname '*.cgl' | sed "s/.*\///"| sed -e 's/\.[^./]*$//' | parallel ./run.sh {}
+# A specific sequence
+# parallel ./run.sh Level{} ::: {11..20} 
